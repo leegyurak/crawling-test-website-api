d1ca6eb67f466f7ca268bac5c5d72edd48cc4fd8
이규락 <62545703+leegyurak@users.noreply.github.com>
2022-09-07 15:47:24 +0900

    [FIX] change db table name

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,8 @@ class Post(models.Model):
     link = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = 'post'
+
     def __str__(self):
         return self.title
