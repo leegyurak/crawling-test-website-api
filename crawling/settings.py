027cbb3f3126c8cf92830fb3dc1fdb942185d7c3
이규락 <62545703+leegyurak@users.noreply.github.com>
2022-09-07 15:34:37 +0900

    [FIX] change DBMS clearDB to sqlite3

--- crawling/settings.py
+++ crawling/settings.py
@@ -10,23 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import json
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# For using private data
-with open('private.json') as private:
-    private_data = json.load(private)
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = private_data.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-ec)1%tle$zrofitm^^%!*s$^732t061u6@+5-btt*x!bi^a@wy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -84,7 +79,12 @@ WSGI_APPLICATION = 'crawling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = private_data.get('DATABASE')
+DATABASES = DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
