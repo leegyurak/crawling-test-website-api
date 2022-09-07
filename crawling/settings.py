125b92b1f57be4bbe2bd20870a2913330eb1e361
이규락 <62545703+leegyurak@users.noreply.github.com>
2022-09-07 15:43:46 +0900

    [FIX] change debug true

--- crawling/settings.py
+++ crawling/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ec)1%tle$zrofitm^^%!*s$^732t061u6@+5-btt*x!bi^a@wy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
