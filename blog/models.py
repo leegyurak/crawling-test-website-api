3ae13e4a239f05748cd2ee0c316cf564fb6511f4
이규락 <62545703+leegyurak@users.noreply.github.com>
2022-09-07 15:37:55 +0900

    [FEAT] add index

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=127)
-    text = models.TextField(max_length=1023)
+    title = models.CharField(max_length=127, db_index=True)
+    text = models.TextField(max_length=1023, db_index=True)
     link = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
