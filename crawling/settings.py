ff42ecbf1f80d4a866c62e9ba1339105ce9bcd14
이규락 <62545703+leegyurak@users.noreply.github.com>
2022-09-14 01:31:00 +0900

    [FIX] change databases

--- crawling/settings.py
+++ crawling/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'crawling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {'default': None}
+DATABASES = {}
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
